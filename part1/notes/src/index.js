70de2d63e454f214c26352f739647fc6138be04a
pretrator <srivastavujjawal123@gmail.com>
2019-10-04 13:18:38 +0530

    initial changes

---
+++ part1/notes/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+const App=()=>{
+
+    return (
+        <div>
+            <h1> Hi its working</h1>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
